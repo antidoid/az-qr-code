568c1b13dcd54d0f7a9a4a24f1dd4568716d4e9d
antidoid <singhpiyush998@gmail.com>
2024-01-30 14:55:26 +0530

    Update: Footer logos

--- frontend/src/containers/Footer.tsx
+++ frontend/src/containers/Footer.tsx
@@ -1,7 +1,15 @@
+import AzureFunctionLogo from "../assets/functions.svg";
+import AzureBlobStorageLogo from "../assets/blob.svg";
+
 export default function Footer() {
     return (
         <div className="bg-black h-12 flex items-center justify-end">
-            <p className="text-white mr-4">Developed with ❤️</p>
+            <p className="text-white mr-4">
+                Developed with{" "}
+                <img src={AzureBlobStorageLogo} className="inline h-[30px] mx-2" />
+                +
+                <img src={AzureFunctionLogo} className="inline h-[25px] ml-2" />
+            </p>
         </div>
     );
 }
